62028110aa12ec465474023cbc555f60512953f7
João Vitor <joaovitoralmeidas@hotmail.com>
2022-12-16 20:34:35 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/com/joaovitor/curso/entities/User.java
+++ src/main/java/com/joaovitor/curso/entities/User.java
@@ -2,17 +2,26 @@ package com.joaovitor.curso.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	public User() {
-		
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -27,6 +36,7 @@ public class User implements Serializable{
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -34,6 +44,7 @@ public class User implements Serializable{
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -41,6 +52,7 @@ public class User implements Serializable{
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -48,6 +60,7 @@ public class User implements Serializable{
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
@@ -55,6 +68,7 @@ public class User implements Serializable{
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -83,7 +97,4 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
 }
