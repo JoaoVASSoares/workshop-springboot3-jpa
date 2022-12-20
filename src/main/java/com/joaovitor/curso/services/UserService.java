b3f0d312dd57ff35631528b26ebfacdb6c9a6d56
João Vitor <joaovitoralmeidas@hotmail.com>
2022-12-20 20:05:02 -0300

    Camada de serviço, registro de componentes

---
+++ src/main/java/com/joaovitor/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.joaovitor.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaovitor.curso.entities.User;
+import com.joaovitor.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
