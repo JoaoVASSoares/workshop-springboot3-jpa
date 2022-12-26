c0d0da765e7384aab512d56a265f3230ac071615
João Vitor <joaovitoralmeidas@hotmail.com>
2022-12-26 19:44:49 -0300

    Entidade Product

--- src/main/java/com/joaovitor/curso/entities/Product.java
+++ src/main/java/com/joaovitor/curso/entities/Product.java
@@ -12,43 +12,80 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+	private String description;
+	private Double price;
+	private String imgUrl;
+
+	// set é um conjunto, foi feito para garantir que um produto não tenha mais de
+	// uma categoria
+	// instanciada para garantir que comece vazia
 	@Transient
-	private Set<Product> products = new HashSet<>();
-	
-	public Category() {
-		
+	private Set<Category> Categories = new HashSet<>();
+
+	public Product() {
+
 	}
-	public Category(Long id, String name) {
+
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
 	}
 
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return description;
 	}
-	
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return Categories;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -64,7 +101,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -72,7 +109,4 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-
-	
-	
 }
---
+++ src/main/java/com/joaovitor/curso/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.joaovitor.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaovitor.curso.entities.Product;
+import com.joaovitor.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
