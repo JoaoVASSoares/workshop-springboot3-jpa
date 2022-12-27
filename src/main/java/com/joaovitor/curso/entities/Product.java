af7df08fa01377f2f7b7ac8fd3e503594c866ef2
João Vitor <joaovitoralmeidas@hotmail.com>
2022-12-27 19:23:05 -0300

    Associação muitos-para-muitos com JoinTable

--- src/main/java/com/joaovitor/curso/entities/Product.java
+++ src/main/java/com/joaovitor/curso/entities/Product.java
@@ -8,14 +8,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -27,8 +29,9 @@ public class Product implements Serializable {
 	// set é um conjunto, foi feito para garantir que um produto não tenha mais de
 	// uma categoria
 	// instanciada para garantir que comece vazia
-	@Transient
-	private Set<Category> Categories = new HashSet<>();
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
 
@@ -83,7 +86,7 @@ public class Product implements Serializable {
 	}
 
 	public Set<Category> getCategories() {
-		return Categories;
+		return categories;
 	}
 
 	@Override
@@ -93,6 +96,7 @@ public class Product implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
