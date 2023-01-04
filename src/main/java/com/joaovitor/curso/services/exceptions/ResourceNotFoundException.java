b92a3bab2440fd17b1da984e966aae67f40b818c
João Vitor <joaovitoralmeidas@hotmail.com>
2023-01-04 20:28:35 -0300

    Tratamento de exceção - findById

---
+++ src/main/java/com/joaovitor/curso/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.joaovitor.curso.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+}
