acafba1dc47b06e97ea25c4ebab9974529e898e4
João Vitor <joaovitoralmeidas@hotmail.com>
2023-01-07 11:28:53 -0300

    Tratamento de exceção - update - Correção

--- src/main/java/com/joaovitor/curso/services/UserService.java
+++ src/main/java/com/joaovitor/curso/services/UserService.java
@@ -13,6 +13,8 @@ import com.joaovitor.curso.repositories.UserRepository;
 import com.joaovitor.curso.services.exceptions.DatabaseException;
 import com.joaovitor.curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -50,8 +52,7 @@ public class UserService {
 		updateData(entity, obj);
 		return repository.save(entity);
 		}
-		catch(RuntimeException e) {
-			e.printStackTrace();
+		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
 		}
 	}
